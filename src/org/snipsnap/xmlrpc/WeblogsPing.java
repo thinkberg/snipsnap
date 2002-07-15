5c87497b070db2aa798e726af61ef718883c2055
stephan <stephan@3c4f0742-5df2-0310-afec-de16d066d075>
2002-07-15 15:29:51 +0000

    Initial release.

---
+++ src/org/snipsnap/xmlrpc/WeblogsPing.java
@@ -0,0 +1,53 @@
+/*
+ * This file is part of "SnipSnap Wiki/Weblog".
+ *
+ * Copyright (c) 2002 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://snipsnap.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * --LICENSE NOTICE--
+ */
+
+package com.neotis.xmlrpc;
+
+import org.apache.xmlrpc.XmlRpcClient;
+
+import java.util.Vector;
+
+/**
+ * Pings weblogs.com
+ *
+ * @author stephan
+ * @version $Id$
+ */
+
+public class WeblogsPing {
+  public static void ping() {
+    try {
+      XmlRpcClient xmlrpc = new XmlRpcClient("http://www.weblogs.com/RPC2");
+      Vector params = new Vector();
+      // @TODO read name and url from configuration
+      params.addElement("SnipSnap");
+      params.addElement("http://www.snipsnap.org");
+      xmlrpc.execute("weblogUpdates.ping", params);
+    } catch (Exception e) {
+      System.err.println("Unable to ping weblogs.com");
+    }
+  }
+
+}
