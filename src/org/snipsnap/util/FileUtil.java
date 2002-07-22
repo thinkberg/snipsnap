521c30994d1365fd306b37496d67224c55ae06b6
leo <leo@3c4f0742-5df2-0310-afec-de16d066d075>
2002-07-22 11:04:01 +0000

    *** empty log message ***

--- src/org/snipsnap/util/FileUtil.java
+++ src/org/snipsnap/util/FileUtil.java
@@ -24,18 +24,51 @@
  */
 package com.neotis.util;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.zip.Adler32;
+import java.util.zip.CheckedInputStream;
 
 /**
- * 
+ *
  * @author Matthias L. Jugel
  * @version $Id$
  */
 public class FileUtil {
 
-  public static Checksum checksumDirectory(File file) {
-    if(file.isDirectory()) {
-      Checksum checksum = new Checksum(file.getAbsolutePath());
+  public static Checksum checksumDirectory(File file) throws IOException {
+    Checksum checksum = new Checksum(file.getAbsolutePath());
+    if (file.isDirectory()) {
+      checksumFiles(file, file.getPath(), checksum);
+    } else {
+      checksumFile(file, file.getPath(), checksum);
     }
+    return checksum;
+  }
+
+
+  private static void checksumFiles(File file, String root, Checksum checksum) throws IOException {
+    File files[] = file.listFiles();
+    for (int i = 0; i < files.length; i++) {
+      if (files[i].isDirectory()) {
+        checksumFiles(files[i], root, checksum);
+      } else {
+        checksumFile(file, root, checksum);
+      }
+    }
+  }
+
+  private static void checksumFile(File file, String root, Checksum checksum) throws IOException {
+    CheckedInputStream fin = new CheckedInputStream(new BufferedInputStream(new FileInputStream(file)),
+                                                    new Adler32());
+    byte buffer[] = new byte[8192];
+    while ((fin.read(buffer)) != -1) {
+      /* ignore ... */
+    }
+    Long checkSum = new Long(fin.getChecksum().getValue());
+    checksum.add(file.getAbsolutePath(), checkSum);
+    fin.close();
   }
 }
