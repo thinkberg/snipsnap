7da0ad4e23fb44b1aca36586f8d1eaaa596321e3
stephan <stephan@3c4f0742-5df2-0310-afec-de16d066d075>
2002-07-23 13:37:56 +0000

    Initial release.

---
+++ src/org/snipsnap/net/SnipLockServlet.java
@@ -0,0 +1,66 @@
+/*
+ * This file is part of "SnipSnap Wiki/Weblog".
+ *
+ * Copyright (c) 2002 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://snipsnap.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * --LICENSE NOTICE--
+ */
+package com.neotis.net;
+
+import com.neotis.app.Application;
+import com.neotis.snip.Snip;
+import com.neotis.snip.SnipLink;
+import com.neotis.snip.SnipSpace;
+import com.neotis.user.Permissions;
+import com.neotis.user.Roles;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Servlet to lock and unlock snips
+ * @author Stephan J. Schmidt
+ * @version $Id$
+ */
+public class SnipLockServlet extends HttpServlet {
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    HttpSession session = request.getSession(false);
+
+    String name = request.getParameter("name");
+    SnipSpace space = SnipSpace.getInstance();
+    Snip snip = space.load(name);
+    if (session != null) {
+      Application app = Application.getInstance(session);
+      if (request.getParameter("unlock") != null) {
+        snip.getPermissions().remove(Permissions.EDIT, Roles.OWNER);
+      } else {
+        snip.setCUser(app.getUser());
+        snip.getPermissions().add(Permissions.EDIT, Roles.OWNER);
+      }
+      space.store(snip);
+    }
+    response.sendRedirect(SnipLink.absoluteLink(request, "/space/" + snip.getName()));
+  }
+}
