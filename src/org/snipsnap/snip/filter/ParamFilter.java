d7387a3942e0102abbc0556b113b2cb7792098b5
stephan <stephan@3c4f0742-5df2-0310-afec-de16d066d075>
2002-07-25 15:26:05 +0000

    Initial release.

---
+++ src/org/snipsnap/snip/filter/ParamFilter.java
@@ -0,0 +1,75 @@
+/*
+ * This file is part of "SnipSnap Wiki/Weblog".
+ *
+ * Copyright (c) 2002 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://snipsnap.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * --LICENSE NOTICE--
+ */
+/*
+ * LinkFilter finds [text] in its input and transforms this
+ * to <link name="text">
+ *
+ * @author stephan
+ * @team sonicteam
+ * @version $Id$
+ */
+package org.snipsnap.snip.filter;
+
+import org.apache.oro.text.regex.MatchResult;
+import org.snipsnap.snip.Snip;
+import org.snipsnap.snip.filter.regex.RegexTokenFilter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ParamFilter extends RegexTokenFilter {
+  private Map params;
+
+  public void handleMatch(StringBuffer buffer, MatchResult result, Snip snip) {
+    // String[] params = null;
+    // String content = null;
+    String name = result.group(1);
+    if (params.containsKey(name)) {
+      buffer.append(params.get(name));
+    } else {
+      buffer.append("<");
+      buffer.append(name);
+      buffer.append(">");
+    }
+  }
+
+  public ParamFilter(String[] paramArray) {
+    this();
+    params = new HashMap();
+    int size = paramArray.length;
+    for (int i = 0; i < size; i++) {
+      params.put("" + (i + 1), paramArray[i]);
+    }
+  }
+
+  public ParamFilter() {
+    super("\\{\\$([^}]*)\\}", SINGLELINE);
+  }
+
+  public ParamFilter(Map params) {
+    this();
+    this.params = params;
+  };
+}
---
+++ src/org/snipsnap/user/SessionListener.java
@@ -0,0 +1,50 @@
+/*
+ * This file is part of "SnipSnap Wiki/Weblog".
+ *
+ * Copyright (c) 2002 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://snipsnap.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * --LICENSE NOTICE--
+ */
+
+package org.snipsnap.user;
+
+import org.snipsnap.app.Application;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Gets called when a session gets a timeout and removes the
+ * user of the timedout session from the current user list
+ *
+ * @author stephan
+ * @version $Id$
+ */
+
+public class SessionListener implements HttpSessionListener {
+  public void sessionCreated(HttpSessionEvent event) {
+  }
+
+  public void sessionDestroyed(HttpSessionEvent event) {
+    HttpSession session = event.getSession();
+    Application.removeCurrentUser(session);
+  }
+}
