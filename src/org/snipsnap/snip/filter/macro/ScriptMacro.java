9228418e39c4f550a37b6df0b96552da63cbf92d
stephan <stephan@3c4f0742-5df2-0310-afec-de16d066d075>
2002-09-16 12:03:55 +0000

    Added python scripting.

---
+++ src/org/snipsnap/snip/filter/macro/ScriptMacro.java
@@ -0,0 +1,69 @@
+/*
+ * This file is part of "SnipSnap Wiki/Weblog".
+ *
+ * Copyright (c) 2002 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://snipsnap.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * --LICENSE NOTICE--
+ */
+/*
+ * Macro that replaces external links
+ *
+ * @author stephan
+ * @team sonicteam
+ * @version $Id$
+ */
+
+package org.snipsnap.snip.filter.macro;
+
+import org.snipsnap.snip.Snip;
+import org.snipsnap.snip.filter.Filter;
+import org.snipsnap.snip.filter.JavaCodeFilter;
+import org.snipsnap.snip.filter.SqlCodeFilter;
+import org.snipsnap.snip.filter.XmlCodeFilter;
+import org.python.util.PythonInterpreter;
+import org.python.core.PyObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.StringWriter;
+
+public class ScriptMacro extends Preserved {
+  private Map filters;
+
+  public ScriptMacro() {
+  }
+
+  public String getName() {
+    return "script";
+  }
+
+  public void execute(StringBuffer buffer, String[] params, String content, Snip snip) throws IllegalArgumentException {
+     PythonInterpreter interp =
+	    new PythonInterpreter();
+
+    StringWriter sw = new StringWriter();
+
+    interp.setOut(sw);
+    interp.set("snip", snip);
+    interp.exec(content);
+
+    buffer.append(sw.getBuffer());
+  }
+}
