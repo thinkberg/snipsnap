ff7248431a87379cdde74a7e943572e3e883388f
leo <leo@3c4f0742-5df2-0310-afec-de16d066d075>
2002-09-26 13:51:59 +0000

    fixed shell scripts

--- db.sh
+++ db.sh
@@ -1,3 +1,8 @@
+#! /bin/sh
+if [ ! $# == 3 ]; then
+  echo usage: $0 user password application
+  exit
+fi
 java -cp lib/mckoidb.jar com.mckoi.tools.JDBCQueryTool \
        -url "jdbc:mckoi:local://./applications/$3/db.conf" \
        -u "$1" -p "$2"
--- dbrepair.sh
+++ dbrepair.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+if [ ! $# == 1 ]; then
+  echo usage: $0 application
+  exit
+fi
 java -cp lib/mckoidb.jar com.mckoi.tools.DBConglomerateRepairTool -path applications/$1/db/data
