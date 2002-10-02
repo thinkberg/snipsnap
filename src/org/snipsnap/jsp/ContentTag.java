919704d76ce30190bb91428cb8ec8dbf10e39121
stephan <stephan@3c4f0742-5df2-0310-afec-de16d066d075>
2002-10-02 12:32:07 +0000

    Initial release.

---
+++ src/org/snipsnap/jsp/ContentTag.java
@@ -0,0 +1,90 @@
+/*
+ * This file is part of "SnipSnap Wiki/Weblog".
+ *
+ * Copyright (c) 2002 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://snipsnap.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * --LICENSE NOTICE--
+ */
+package org.snipsnap.jsp;
+
+import org.apache.taglibs.standard.lang.support.ExpressionEvaluatorManager;
+import org.snipsnap.snip.Snip;
+import org.snipsnap.snip.filter.Filter;
+import org.snipsnap.snip.filter.HtmlRemoveFilter;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.jstl.core.ConditionalTagSupport;
+import javax.servlet.jsp.tagext.TagSupport;
+import java.io.IOException;
+
+/*
+ * Tag that displays the content of a snip. Can remove HTML from the content
+ * and generate an extract.
+ *
+ * @author Stephan J. Schmidt
+ * @version $Id$
+ */
+
+public class ContentTag extends TagSupport {
+  Snip snip = null;
+  boolean extract = false;
+  boolean removeHtml = true;
+
+  public int doStartTag() throws JspException {
+    if (null != snip) {
+      String content = snip.getXMLContent();
+      if (removeHtml) {
+        Filter filter = new HtmlRemoveFilter();
+        content = filter.filter(content, null);
+        if (extract) {
+          if (content.length() >40 ) {
+            content = content.substring(0,40) + " ...";
+          }
+        }
+      }
+
+
+      try {
+        JspWriter out = pageContext.getOut();
+        out.print(content);
+      } catch (IOException e) {
+        System.err.println("doStartTag in ContentTag: " + e);
+      }
+    }
+    return super.doStartTag();
+  }
+
+  public void setExtract(boolean extract) {
+    this.extract = extract;
+  }
+
+  public void setRemoveHtml(boolean removeHtml) {
+    this.removeHtml = removeHtml;
+  }
+
+  public void setSnip(String snip) {
+    try {
+      this.snip = (Snip) ExpressionEvaluatorManager.evaluate("snip", snip, Snip.class, this, pageContext);
+    } catch (JspException e) {
+      System.err.println("unable to evaluate expression: " + e);
+    }
+  }
+}
