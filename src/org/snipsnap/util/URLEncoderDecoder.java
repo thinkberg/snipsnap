44216178890fc34ecebf0d17e07a8953e07baba2
leo <leo@3c4f0742-5df2-0310-afec-de16d066d075>
2002-10-14 08:53:23 +0000

    added header

--- src/org/snipsnap/util/URLEncoderDecoder.java
+++ src/org/snipsnap/util/URLEncoderDecoder.java
@@ -1,8 +1,39 @@
+/*
+ * This file is part of "SnipSnap Wiki/Weblog".
+ *
+ * Copyright (c) 2002 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://snipsnap.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * --LICENSE NOTICE--
+ */
+
 package org.snipsnap.util;
 
 import java.util.BitSet;
 import java.util.StringTokenizer;
 
+/**
+ * Replacement for URLEncoder/URLDecoder of the standard SDK. This was necessary,
+ * as the default encoder/decoder used always the platform encoding, not ISO-8859-1
+ * @author Matthias L. Jugel
+ * @version $Id$
+ */
 public class URLEncoderDecoder {
 
   static BitSet dontNeedEncoding;
