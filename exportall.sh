7c1a690172c06d74252d0b2c4135a1151c56ae6e
leo <leo@3c4f0742-5df2-0310-afec-de16d066d075>
2002-11-13 15:04:47 +0000

    *** empty log message ***

--- exportall.sh
+++ exportall.sh
@@ -3,6 +3,7 @@ base=`dirname $0`
 for appdir in $base/applications/*
 do
   app=`basename $appdir`
-  echo "Exporting $app to $base/app/$app/$app.snip ..."
-  $base/dbexport.sh -config $base/applications/$app/application.conf > $base/applications/$app/$app.snip 2>$base/applications/$app/export.err
+  echo "Exporting $app to $appdir/WEB-INF/$app.snip ..."
+  $base/dbexport.sh -config $appdir/WEB-INF/application.conf \
+	> $appdir/WEB-INF/$app.snip 2>$appdir/WEB-INF/export.err
 done
