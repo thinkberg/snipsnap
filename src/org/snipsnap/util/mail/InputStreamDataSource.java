56563bec9efa24960ee4bd610f7c122c33c6cde4
leo <leo@3c4f0742-5df2-0310-afec-de16d066d075>
2002-12-19 14:43:36 +0000

    *** empty log message ***

---
+++ src/org/snipsnap/util/mail/InputStreamDataSource.java
@@ -0,0 +1,61 @@
+/*
+ * This file is part of "SnipSnap Wiki/Weblog".
+ *
+ * Copyright (c) 2002 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://snipsnap.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * --LICENSE NOTICE--
+ */
+package org.snipsnap.util.mail;
+
+import javax.activation.DataSource;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * A datasource for InputStream type sources.
+ * @author Matthias L. Jugel
+ * @version $Id$
+ */
+public class InputStreamDataSource implements DataSource {
+  InputStream inputStream = null;
+  String contentType = null;
+
+  public InputStreamDataSource(InputStream in, String type) {
+    inputStream = in;
+    contentType = type;
+  }
+
+  public String getContentType() {
+    return contentType;
+  }
+
+  public InputStream getInputStream() throws IOException {
+    return inputStream;
+  }
+
+  public String getName() {
+    return "unknown";
+  }
+
+  public OutputStream getOutputStream() throws IOException {
+    throw new IOException("InputStreamDataSource is read only");
+  }
+}
