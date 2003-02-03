d29fe7672eb5e3049bd0079a97e89c66fb1caa20
stephan <stephan@3c4f0742-5df2-0310-afec-de16d066d075>
2003-02-03 14:39:15 +0000

    Initial release.

---
+++ src/org/snipsnap/render/macro/parameter/SnipMacroParameter.java
@@ -0,0 +1,71 @@
+/*
+ * This file is part of "SnipSnap Wiki/Weblog".
+ *
+ * Copyright (c) 2002 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://snipsnap.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * --LICENSE NOTICE--
+ */
+
+package org.snipsnap.render.macro.parameter;
+
+import org.snipsnap.app.Application;
+import org.snipsnap.snip.Snip;
+import org.radeox.util.logging.Logger;
+import org.radeox.macro.parameter.BaseMacroParameter;
+import org.radeox.macro.parameter.MacroParameter;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/**
+ * Encapsulates parameters for an execute Macro call.
+ * This extends MacroParameter for usage in a SnipSnap
+ * enviroment. For example SnipMacroParameter contains
+ * a getSnip() method to get the current Snip
+ *
+ * @author Stephan J. Schmidt
+ * @version $Id$
+ */
+
+public class SnipMacroParameter extends BaseMacroParameter implements MacroParameter {
+  private Snip snip;
+
+
+  public SnipMacroParameter(Snip snip) {
+    params = new HashMap();
+    this.snip = snip;
+  }
+
+  public SnipMacroParameter(String stringParams) {
+    setParams(stringParams);
+  }
+
+  /**
+   * Gets the current snip from which the macro was called.
+   *
+   * @return snip Snip from which the macro was called
+   */
+  public Snip getSnip() {
+    return snip;
+  }
+
+
+}
