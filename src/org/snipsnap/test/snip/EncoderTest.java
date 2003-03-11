038dcda6de1b34ecc34560364087aa432c49ba9a
leo <leo@3c4f0742-5df2-0310-afec-de16d066d075>
2003-03-11 13:33:35 +0000

    UTF8 tests working

---
+++ src/org/snipsnap/test/snip/EncoderTest.java
@@ -0,0 +1,72 @@
+/*
+ * This file is part of "SnipSnap Wiki/Weblog".
+ *
+ * Copyright (c) 2002 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://snipsnap.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * --LICENSE NOTICE--
+ */
+
+package org.snipsnap.test.snip;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.snipsnap.snip.SnipLink;
+import org.snipsnap.snip.SnipImpl;
+import org.snipsnap.snip.Snip;
+import org.snipsnap.util.URLEncoderDecoder;
+
+import java.io.Writer;
+import java.io.StringWriter;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class EncoderTest extends SnipTestSupport {
+  public EncoderTest(String name) {
+    super(name);
+  }
+
+  String unencodedString = null;
+  String encodedString = null;
+
+  protected void setUp() throws Exception {
+    super.setUp();
+    // the text below is complete nonsense, randomly typed
+    unencodedString = new String("سىزذتازىذتازسذ.شسذز.سىازذتا".getBytes(), "UTF-8");
+    encodedString = "%D8%B3%D9%89%D8%B2%D8%B0%D8%AA%D8%A7%D8%B2%D9%89%D8%B0%D8%AA%D8%A7%D8%B2%D8%B3%D8%B0.%D8%B4%D8%B3%D8%B0%D8%B2.%D8%B3%D9%89%D8%A7%D8%B2%D8%B0%D8%AA%D8%A7";
+
+  }
+
+  public static Test suite() {
+    return new TestSuite(EncoderTest.class);
+  }
+
+  public void testUTF8Encoding() throws UnsupportedEncodingException {
+    assertEquals(encodedString, URLEncoderDecoder.encode(unencodedString, "UTF-8"));
+  }
+
+  public void testUTF8Decoding() throws UnsupportedEncodingException  {
+    assertEquals(unencodedString, URLEncoderDecoder.decode(encodedString, "UTF-8"));
+  }
+
+  public void testCutLength() throws UnsupportedEncodingException {
+    assertEquals("<a href=\"http://snipsnap.org:8668/space/"+encodedString+"\">"+unencodedString.substring(0, 22)+"...</a>",
+                 SnipLink.createLink(unencodedString, SnipLink.cutLength(unencodedString, 25)));
+  }
+}
