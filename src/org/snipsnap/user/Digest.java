48c31f094325d0b76173611554a3ce419dc28743
leo <leo@3c4f0742-5df2-0310-afec-de16d066d075>
2003-07-30 09:47:29 +0000

    fixed bug that caused an error on first start

--- src/org/snipsnap/user/Digest.java
+++ src/org/snipsnap/user/Digest.java
@@ -47,7 +47,7 @@ public class Digest {
   }
 
   public static String getDigest(String s) {
-    if (digest != null) {
+    if (digest != null && s != null) {
       return digestToHexString(digest.digest(s.getBytes()));
     }
     return "";
