b36bf8ac3a588eeb0a22a6b7cfe68c82c3a0f31a
stephan <stephan@3c4f0742-5df2-0310-afec-de16d066d075>
2003-08-07 08:24:58 +0000

    Added

---
+++ src/org/snipsnap/interceptor/custom/MissingSnipAspect.java
@@ -0,0 +1,102 @@
+package org.snipsnap.interceptor.custom;
+
+/*
+ * This file is part of "SnipSnap Wiki/Weblog".
+ *
+ * Copyright (c) 2002-2003 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://snipsnap.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * --LICENSE NOTICE--
+ */
+
+import org.codehaus.nanning.AspectInstance;
+import org.codehaus.nanning.Invocation;
+import org.codehaus.nanning.MethodInterceptor;
+import org.codehaus.nanning.config.Aspect;
+import org.codehaus.nanning.config.P;
+import org.codehaus.nanning.config.Pointcut;
+import org.snipsnap.snip.SnipSpace;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * MissingLSnip Aspect caches method call result
+ * from SnipSpace (exists, create).
+ * When a missing snip is detected the
+ * snip name is stored. When the snip is created
+ * the snip is removed from the missing list.
+ *
+ * @author stephan
+ * @version $Id$
+ */
+
+public class MissingSnipAspect implements Aspect {
+  Pointcut existsPc = P.methodName("exists.*");
+  Pointcut createPc = P.methodName("create.*");
+
+  private Map missing;
+
+  public void advise(AspectInstance instance) {
+    Class klass = instance.getClassIdentifier();
+    System.out.println("class="+klass);
+    System.out.println("instance="+instance);
+    if (klass != null && klass.equals(SnipSpace.class)) {
+
+      existsPc.advise(instance, new MethodInterceptor() {
+        public Object invoke(Invocation invocation) throws Throwable {
+          String name = ((String)
+              invocation.getArgs()[0]).toUpperCase();
+          // Snip is in the missing list
+          if (missing.containsKey(name)) {
+            System.out.println("Miss.");
+            return new Boolean(false);
+          }
+          Boolean result = (Boolean)
+              invocation.invokeNext();
+          // The snip does not exist so put it in the missing list
+          if (result.equals(Boolean.FALSE)) {
+            missing.put(name, new Integer(0));
+          }
+          return result;
+        }
+      });
+
+      createPc.advise(instance, new MethodInterceptor() {
+        public Object invoke(Invocation invocation) throws Throwable {
+          String name = ((String) invocation.getArgs()[0]).toUpperCase();
+
+          Object result = invocation.invokeNext();
+
+          if (missing.containsKey(name)) {
+            missing.remove(name);
+          }
+          return result;
+        }
+      });
+    }
+  }
+
+  public MissingSnipAspect() {
+    this.missing = new HashMap();
+  }
+
+  public void introduce(AspectInstance instance) {
+  }
+}
