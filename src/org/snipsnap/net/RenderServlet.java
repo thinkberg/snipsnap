4863bf130c3bac89081bd7cddd86c99b983f1e1e
stephan <stephan@3c4f0742-5df2-0310-afec-de16d066d075>
2003-09-17 12:58:20 +0000

    Added.

---
+++ src/org/snipsnap/net/RenderServlet.java
@@ -0,0 +1,83 @@
+/*
+ * This file is part of "SnipSnap Wiki/Weblog".
+ *
+ * Copyright (c) 2002 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://snipsnap.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * --LICENSE NOTICE--
+ */
+package org.snipsnap.net;
+
+import org.snipsnap.config.Configuration;
+import org.snipsnap.config.ConfigurationProxy;
+import org.snipsnap.snip.Blog;
+import org.snipsnap.snip.Snip;
+import org.snipsnap.snip.SnipSpaceFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/**
+ * Get some data from a snip and render the content
+ *
+ * @author Stephan J. Schmidt
+ * @version $Id$
+ */
+public class RenderServlet extends HttpServlet {
+
+  public void init(ServletConfig servletConfig) throws ServletException {
+  }
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+
+    String handler = request.getParameter("handler");
+    String name = request.getParameter("name");
+    name = name.replace('+', ' ');
+
+    Snip snip = SnipSpaceFactory.getInstance().load(name);
+    String content = snip.getContent();
+
+    int start = 0;
+    int end = 0;
+    try {
+      start = Integer.parseInt(request.getParameter("start"));
+    } catch (NumberFormatException e) {
+      start = 0;
+    }
+    try {
+      end = Integer.parseInt(request.getParameter("end"));
+    } catch (NumberFormatException e) {
+      end = content.length();
+    }
+
+    ServletOutputStream out = response.getOutputStream();
+
+    response.setContentType("text/plain");
+
+    out.println(snip.getContent().substring(start, end));
+  }
+}
