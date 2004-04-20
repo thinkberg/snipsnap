7bbe4bef63775c4c37d33dc60fcb362b00ca5e1f
leo <leo@3c4f0742-5df2-0310-afec-de16d066d075>
2004-04-20 14:48:48 +0000

    *** empty log message ***

--- run.sh
+++ run.sh
@@ -21,5 +21,7 @@ case "`uname`" in
 esac
 
 JAVA_OPTS="$JAVA_OPTS -DentityExpansionLimit=1000000 -Djava.awt.headless=true -Xmx512m"
+# uncomment if you have a lot of memory and want optimizations
+#JAVA_OPTS="$JAVA_OPTS -Xsqnopause -XX:+UseLWPSynchronization -Xms512m -Xmx1024m -Xss256k -XX:MaxNewSize=96m -XX:MaxPermSize=512m"
 
 $JAVA $JAVA_OPTS $DBG -jar lib/snipsnap.jar $*
