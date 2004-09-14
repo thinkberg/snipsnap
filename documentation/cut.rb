4b0c4335df86177a5db6bc4cfe8cff25b513080d
stephan <stephan@3c4f0742-5df2-0310-afec-de16d066d075>
2004-09-14 12:44:26 +0000

    *** empty log message ***

---
+++ documentation/cut.rb
@@ -0,0 +1,43 @@
+def cut(fileName, start, stop)
+    inHandle = File::open( fileName )
+
+    inText = inHandle.read()
+    result = "ERROR #{fileName}"
+    inText.scan(/\/\/.*?cut:#{start}.*?\n(.*?)\n\/\/[^\/]*cut:#{stop}/m) do |block|
+      lines = block[0].split("\n")
+      gobble = countSpace( lines )
+
+      beginBlock = "<source gobble=\"#{gobble}\">\n"
+      endBlock = "</source>"
+      result = block[0]
+      result = "%!SRC|#{fileName}|#{start}|#{stop}|\n" + "#{beginBlock}"  + result + "\n#{endBlock}\n%!END"
+    end
+    result
+end
+
+def countSpace( lines )
+  count = 1000
+  lines.each do |line|
+       if line.length>0
+       currentCount = 0
+       while line[currentCount,1] == " "
+          currentCount = currentCount + 1
+       end
+       if count > currentCount 
+          count = currentCount
+       end
+     end
+  end
+  count
+end
+
+fileName = ARGV[0]
+fHandle = File::open( fileName )
+inText = fHandle.read()
+found = false
+
+inText.gsub!(/\%\!SRC\|(.*?)\|(.*?)\|(.*?)\|.*?\n(.*?)%!END/m) do |match|
+    result = match.split("|") 
+    cut(result[1], result[2], result[3])
+end
+print inText
