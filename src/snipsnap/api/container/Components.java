87b28b2fbdbd56be8dba0017c40743e216c8d5ee
stephan <stephan@3c4f0742-5df2-0310-afec-de16d066d075>
2005-04-13 09:40:52 +0000

    Moved to API

---
+++ src/snipsnap/api/container/Components.java
@@ -0,0 +1,58 @@
+package snipsnap.api.container;
+
+/*
+ * This file is part of "SnipSnap Wiki/Weblog".
+ *
+ * Copyright (c) 2002 Stephan J. Schmidt, Matthias L. Jugel
+ * All Rights Reserved.
+ *
+ * Please visit http://snipsnap.org/ for updates and contact.
+ *
+ * --LICENSE NOTICE--
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * --LICENSE NOTICE--
+ */
+
+import org.snipsnap.container.Container;
+import org.snipsnap.container.PicoContainer;
+
+import java.util.Collection;
+
+public class Components {
+    public final static String DEFAULT_ENGINE = "defaultRenderEngine";
+
+    private static Container container;
+
+    public static synchronized Container getContainer() {
+        if (null == container) {
+            container = new PicoContainer();
+            container.init();
+        }
+
+        return container;
+    }
+
+    public static Object getComponent(Class c) {
+        return getContainer().getComponent(c);
+    }
+
+    public static Object getComponent(String id) {
+         return getContainer().getComponent(id);
+     }
+
+    public static Collection findComponents(Class c) {
+        return getContainer().findComponents(c);
+    }
+}
